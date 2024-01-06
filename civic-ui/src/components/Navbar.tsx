a71ccb27a97bac16c075db188ee47339b7b96e88
saikarthik.mudigonda@techwave.net <saikarthik.mudigonda@techwave.net>
2024-01-06 23:00:39 +0530

    added navbar

---
+++ civic-ui/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '../styles/Navbar.css' 
+import { faBarsStaggered, faCircleUser } from '@fortawesome/free-solid-svg-icons'
+  function Navbar(){
+    return (
+        <div className='nav-bar'> 
+        <FontAwesomeIcon className='user-icon' icon={faCircleUser} />
+        <div className ="brand-name">
+            CIVIC
+        </div>   
+        <FontAwesomeIcon className='side-bar-icon' icon={faBarsStaggered} />
+        </div>
+
+    )
+  }
+
+  export default Navbar
